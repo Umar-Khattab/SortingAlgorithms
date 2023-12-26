979ffefa7f64724fd155726119a9f840cf4d2275
omar khattab <omar.khattab7@icloud.com>
2023-12-26 18:05:51 +0200

    Add project files.

---
+++ Algorithms.cs
@@ -0,0 +1,102 @@
+﻿namespace SortingAlgorithms
+{
+    internal class Algorithms
+    {
+        public static int[] SimpleSort(int[] nums)
+        {
+            int temp;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
+        public static int[] ReverseSimpleSort(int[] nums)
+        {
+            int temp;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] > nums[i])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
+        public static int[] BubbleSort(int[] nums)
+        {
+            int t;
+            bool f;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                f = false;
+                for (int j = 0; j < nums.Length - 1; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        t = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = t;
+                        f = true;
+                    }
+                }
+                if (!f)
+                    break;
+            }
+            return nums;
+        }
+        static void selectionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            // One by one move boundary of unsorted subarray
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Find the minimum element in unsorted array
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+
+                // Swap the found minimum element with the first
+                // element
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        static void InsertionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                // Move elements of arr[0..i-1],
+                // that are greater than key,
+                // to one position ahead of
+                // their current position
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using SortingAlgorithms;
+
+int[] Arr = new int[] { 147834, 6, 47, 895, 757, 3564, 857 };
+int[] sorted = Algorithms.ReverseSimpleSort(Arr);
+for(int i = 0; i < sorted.Length; i++)
+{
+    Console.Write($"{sorted[i]}  ");
+}
+Console.WriteLine();
